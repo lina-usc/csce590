d08f7ca3ced9ff4c460c4392e00baabaf61a219d
Christian O'Reilly <christian.oreilly@gmail.com>
2024-02-05 11:24:59 -0500

    Update utilfct.py

--- csce590/utilfct.py
+++ csce590/utilfct.py
@@ -5,11 +5,6 @@ import warnings
 
 import numpy as np
 
-from mne import create_info
-from mne.io import RawArray
-from mne.viz.topomap import _add_colorbar
-from mne.viz import plot_topomap
-
 
 def download_file_from_google_drive(id, destination):
     def get_confirm_token(response):
@@ -40,25 +35,24 @@ def download_file_from_google_drive(id, destination):
 
     save_response_content(response, destination)
 
-
 def plot_values_topomap(value_dict, montage, axes, colorbar=True, cmap='RdBu_r',
-                        vmin=None, vmax=None, names=None, image_interp='bilinear', side_cb="right",
-                        sensors=True, show_names=True, **kwargs):
+                        vmin=None, vmax=None, names=None, image_interp='cubic', side_cb="right",
+                        sensors=True, **kwargs):
     if names is None:
         names = [ch_name for ch_name in montage.ch_names if ch_name in value_dict]
 
-    info = create_info(names, sfreq=256, ch_types="eeg")
+    info = mne.create_info(names, sfreq=256, ch_types="eeg")
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        RawArray(np.zeros((len(names), 1)), info, copy=None, verbose=False).set_montage(montage)
+        mne.io.RawArray(np.zeros((len(names), 1)), info, copy=None, verbose=False).set_montage(montage)
 
-    im = plot_topomap([value_dict[ch] for ch in names], pos=info, show=False, image_interp=image_interp,
-                      sensors=sensors, res=64, axes=axes, names=names, show_names=show_names,
-                      vmin=vmin, vmax=vmax, cmap=cmap, **kwargs)
+    im = mne.viz.plot_topomap([value_dict[ch] for ch in names], pos=info, show=False, image_interp=image_interp,
+                      sensors=sensors, res=64, axes=axes, names=names,
+                      vlim=(vmin, vmax), cmap=cmap, **kwargs)
 
     if colorbar:
         try:
-            cbar, cax = _add_colorbar(axes, im[0], cmap, pad=.05,
+            cbar, cax = mne.viz.topomap._add_colorbar(axes, im[0], cmap, pad=.05,
                                       format='%3.2f', side=side_cb)
             axes.cbar = cbar
             cbar.ax.tick_params(labelsize=12)
@@ -67,3 +61,4 @@ def plot_values_topomap(value_dict, montage, axes, colorbar=True, cmap='RdBu_r',
             pass
 
     return im
+
